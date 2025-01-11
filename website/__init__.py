f4dd3d9b4a533baf355b674d5459fd4878c770bc
Ranjan Neupane <ranzanneupane@outlook.com>
2025-01-12 04:25:41 +0900

    Login and Adding Notes

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] ='ILOVEYOU'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -17,14 +19,25 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User,Note
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
 
-    create_db(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-    return app  
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
-def create_db(app):
+    return app
+
+def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Database Created !')
+        with app.app_context():
+            db.create_all()
+        print('Database Created!')
+
 
